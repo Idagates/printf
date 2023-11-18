61be51049095b70ba42b642daf8196306d0ab679
Ida Seye <idamrsgates@gmail.com>
2023-11-17 20:13:04 -0800

    donegit add .git add .git add .!

--- main.h
+++ main.h
@@ -6,6 +6,9 @@
 #include <string.h>
 #include <unistd.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
 int _printf(const char *format, ...);
 int print_string(char *str);
 int print_int(int);
